d889f92517a1ed824c106858af8c80328eb68ab7
madhavpcm <dpmadhav@hotmail.com>
2021-01-18 21:49:41 +0530

    Level 1 Frame Work completed

---
+++ scripts/Dave/Dave.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0, 0)
+var look_direction = Vector2(1,0)
+
+const SPEED = 192
+const JUMPTHRUST = -640
+const GRAVITY = 27
+
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		velocity.x = SPEED
+		look_direction = Vector2(1,0)
+		$Sprite.play("dave_walk")
+		$Sprite.flip_h = false
+		
+	elif Input.is_action_pressed("left"):
+		look_direction = Vector2(-1,0)
+		velocity.x = -SPEED
+		$Sprite.play("dave_walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("dave_idle")
+	
+	if not is_on_floor():
+		$Sprite.play("dave_jump")
+		
+		
+	velocity.y = velocity.y + GRAVITY
+
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor() or Input.is_action_just_released("jump") and is_on_floor():
+		velocity.y = JUMPTHRUST
+		
+	velocity = move_and_slide(velocity, Vector2.UP )
+	
+	velocity.x = lerp(velocity.x , 0 , 0.5)
